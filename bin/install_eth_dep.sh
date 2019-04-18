4f28375f90ba5b6d1af5f3e8cb89640cc0b43f19
Jimmy Hu <jimmy.hu@dexon.org>
2019-04-18 19:00:14 +0800

    misc: fix dep

--- bin/install_eth_dep.sh
+++ bin/install_eth_dep.sh
@@ -4,11 +4,11 @@ if [ -e .dep/libsecp256k1 ]; then
   exit 0
 fi
 
-rm -rf vendor/github.com/dexon-foundation/dexon/crypto/secp256k1/libsecp256k1
+rm -rf vendor/github.com/ethereum/go-ethereum/crypto/secp256k1/libsecp256k1
 if [ ! -d .dep/libsecp256k1 ]; then
   git init .dep/libsecp256k1
   cd .dep/libsecp256k1
-  git remote add origin https://github.com/dexon-foundation/dexon.git
+  git remote add origin https://github.com/ethereum/go-ethereum.git
   git config core.sparsecheckout true
   echo "crypto/secp256k1/libsecp256k1/*" >> .git/info/sparse-checkout
   cd ../../
@@ -17,4 +17,4 @@ cd .dep/libsecp256k1
 git pull --depth=1 origin master
 cd ../../
 cp -r .dep/libsecp256k1/crypto/secp256k1/libsecp256k1 \
-  vendor/github.com/dexon-foundation/dexon/crypto/secp256k1
+  vendor/github.com/ethereum/go-ethereum/crypto/secp256k1
